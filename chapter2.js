25541034cf47f1aefd0d2ea3de93c8658fccb7bc
Taha haider khan <93771060+tahaheyderkhan@users.noreply.github.com>
2025-04-05 11:16:40 +0500

    Add files via upload

---
+++ chapter2.js
@@ -0,0 +1,38 @@
+// 1. Declare a variable called username
+var username;
+
+// 2. Declare a variable called myName and assign full name
+var myName = "John Doe";
+
+// 3. Declare a JS variable, assign "Hello World", and show it in alert
+var message;
+message = "Hello World";
+alert(message);
+
+// 4. Student's bio data in variables and shown using alert
+var studentName = "Ali Khan";
+var studentAge = "20 years old";
+var studentCourse = "Certified Mobile Application Development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// 5. Display pizza pattern using one variable
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// 6. Email address using concatenation
+var email = "example@example.com";
+alert("My email address is " + email);
+
+// 7. Book variable and alert
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// 8. Display a message in browser using JavaScript
+document.write("Yah! I can write HTML content through JavaScript<br>");
+
+// 9. Show special string in alert and in browser
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+document.write(design);
