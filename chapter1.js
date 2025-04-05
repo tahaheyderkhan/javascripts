7d47ee31c1cb1ac790b1424e88ba43c27e25a8fa
Taha haider khan <93771060+tahaheyderkhan@users.noreply.github.com>
2025-04-05 11:18:30 +0500

    Add files via upload

---
+++ chapter1.js
@@ -0,0 +1,10 @@
+alert("Welcome to our website!");
+
+document.write("This is a message shown on the web page.<br>");
+
+document.write("Welcome to JS Land...<br>Happy Coding!<br><br>");
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+console.log("Hello... I can run JS through my web browser’s console");
