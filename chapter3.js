9f097ff1594f6c034cfd2fdc61195e0a2e50a21d
Taha haider khan <93771060+tahaheyderkhan@users.noreply.github.com>
2025-04-05 11:11:03 +0500

    Add files via upload

---
+++ chapter3.js
@@ -0,0 +1,28 @@
+var username;
+var myName = "John Doe";
+
+var message;
+message = "Hello World";
+alert(message);
+
+var studentName = "Ali Khan";
+var studentAge = "20 years old";
+var studentCourse = "Certified Mobile Application Development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+var email = "example@example.com";
+alert("My email address is " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+
+document.write("Yah! I can write HTML content through JavaScript<br>");
+document.write(design);
