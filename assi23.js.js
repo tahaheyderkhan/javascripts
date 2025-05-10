d1d5e1afcfb69fbc881c0340532aad6cecbe829f
Taha haider khan <93771060+tahaheyderkhan@users.noreply.github.com>
2025-05-10 15:22:47 +0500

    Update assi23.js

--- assi23.js.js
+++ assi23.js.js
@@ -8,16 +8,12 @@ alert("Hello, " + fullName + "!");
 
 
 
-
-
 var userInput = prompt("Enter your favorite mobile phone model:");
 
 var inputLength = userInput.length;
 
 document.write("Your favorite phone is: " + userInput + "<br>");
 document.write("Length of your input is: " + inputLength);
-
-
 
 
 
@@ -43,16 +39,12 @@ document.write("Last index of 'l': " + lastIndex);
 
 
 
-
-
 var word = "Pakistani";
 
 var charAtIndex3 = word.charAt(3);
 
 document.write("String: " + word + "<br>");
 document.write("Character at index 3: " + charAtIndex3);
-
-
 
 
 
@@ -80,17 +72,12 @@ document.write("After replacement: " + replacedCity);
 
 
 
-
-
-
 var message = "Ali and Sami are best friends. They play cricket and football together.";
 
 var updatedMessage = message.replace(/and/g, "&");
 
 document.write("Original message: " + message + "<br>");
 document.write("Updated message: " + updatedMessage);
-
-
 
 
 
@@ -106,9 +93,6 @@ document.write("Value: " + num + " (Type: " + typeof num + ")");
 
 
 
-
-
-
 var userInput = prompt("Enter some text:");
 
 var upperCaseInput = userInput.toUpperCase();
@@ -119,25 +103,6 @@ document.write("Upper Case: " + upperCaseInput);
 
 
 
-
-
-
-function toTitleCase(str) {
-    return str.replace(/\b\w/g, function(char) {
-      return char.toUpperCase();
-    });
-  }
-  
-  var userInput = prompt("Enter some text:");
-  
-  var titleCaseInput = toTitleCase(userInput);
-  
-  document.write("User Input: " + userInput + "<br>");
-  document.write("Title Case: " + titleCaseInput);
-
-  
-
-
 var num = 35.36;
 
 var numStr = num.toString();
@@ -146,9 +111,6 @@ var numWithoutDot = numStr.replace(".", "");
 
 document.write("Original number: " + num + "<br>");
 document.write("Converted to string and without dot: " + numWithoutDot);
-
-
-
 
 
 var username = prompt("Enter your username:");
@@ -166,27 +128,10 @@ function isValidUsername(username) {
   return true; 
 }
 
-if (isValidUsername(username)) {
-  document.write("Your username is valid: " + username);
-} else {
-  alert("Invalid username. Please enter a valid username without any special symbols (@, ., ,, !).");
-}
 
 
 
 
 
 
-var A = ["cake", "apple pie", "cookie", "chips", "patties"];
 
-var userInput = prompt("Enter the item you want to search for:").toLowerCase(); 
-
-function searchItem(arr, item) {
-  return arr.some(element => element.toLowerCase() === item);
-}
-
-if (searchItem(A, userInput)) {
-  alert(userInput + " is found in the list.");
-} else {
-  alert(userInput + " is not found in the list.");
-}
